595cfec273c1e3231d5ff11f6b1057031d39dd71
Verzach3 <gabrielcorreacardenas@gmail.com>
2026-02-28 17:16:54 -0500

    feat(db): improve db schema

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,1 +1,6 @@
-export * from "./auth.schema.ts"
+export * from "./auth.schema";
+export * from "./credit.schema";
+export * from "./customer.schema";
+export * from "./inventory.schema";
+export * from "./pos.schema";
+export * from "./sales.schema";
